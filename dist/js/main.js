8947d669ee0bc24136944a1cec1554726ed158eb
Stephen <75111371+Oha-Stephen@users.noreply.github.com>
2021-01-25 11:09:07 -0800

    Update main.js

--- dist/js/main.js
+++ dist/js/main.js
@@ -26,3 +26,21 @@ function toggleMenu(){
         showMenu = false;
     }
 }
+
+let mywindow = $(window);
+let mypos = mywindow.scrollTop();
+let up = false;
+let newscroll;
+mywindow.scroll(function(){
+    newscroll = mywindow.scrollTop();
+    if(newscroll > mypos && !up){
+        $('.navigationBar').stop().fadeOut();
+        up = !up;
+        console.log(up);
+
+    }else if(newscroll < mypos && up){
+        $('.navigationBar').stop().fadeIn();
+        up = !up;
+    }
+    mypos = newscroll;
+});
